0dc29659ee45db0022a3207a21f750244f32ab36
Maria-Gomez <marijose19_94@hotmail.com>
2019-10-25 19:42:04 -0300

    Test 16

--- tests/characters/Elf.cs
+++ tests/characters/Elf.cs
@@ -7,6 +7,7 @@ namespace RoleplayGame.Library.Test
     public class ElfTest
     {
         Elf elf;
+        //🥉3. La creación de un Elf le asigna vida 120
         [Test]
         public void TestAsignarVida()
         {
@@ -15,7 +16,7 @@ namespace RoleplayGame.Library.Test
             int expectedHelth = 120;
             Assert.AreEqual(expectedHelth, elf.Health);
         }
-
+        //🥉3. La creación de un Elf le asigna un item Robes.
         [Test]
         public void TestAsignarRobes()
         {
@@ -33,16 +34,19 @@ namespace RoleplayGame.Library.Test
             }
             Assert.AreEqual(expectedRobes, robes);
         }
+        //🥉6. Cuando un character tiene vida 0, su propiedad isDead es verdadera;
+        // de lo contrario es falsa.
         [Test]
         public void TestVidaCeroIsDeathFalse()
         {
             elf = new Elf("El elfo");
-            elf.ReceiveAttack(120);
+            elf.ReceiveAttack(130);
 
-            bool expected = false;
+            bool expected = true;
             Assert.AreEqual(expected, elf.IsDead);
         }
-
+        //🥈12. Al crear un personaje y asignarle dos items de 
+        //defensa, el poder de defensa la suma de los poderes de ataque de ambos.
         [Test]
         public void TestPoderDefensa()
         {
@@ -67,9 +71,26 @@ namespace RoleplayGame.Library.Test
                     {
                         expectedDefensePower = expectedDefensePower + ((MagicStick)item).DefensePower;
                     }
+                    if(item is Armor)
+                    {
+                        expectedDefensePower = expectedDefensePower + ((Armor)item).DefensePower;
+                    }
                 }
             }            
             Assert.AreEqual(expectedDefensePower, elf.DefensePower);
+        }
+        //🥈16. Cuando un personaje recibe un ataque y 
+        //su defensa es mayor al ataque, su vida no se modifica.
+        [Test]
+        public void TestVidaNoModificada()
+        {
+            Elf elfo = new Elf("El elfo");
+            int expected = elfo.Health;
+
+            elfo.ReceiveAttack(elfo.DefensePower - 1);
+
+            
+            Assert.AreEqual(expected, elfo.Health);
         }
 
     }
